376e97f09f8bc2dbe777c8b95d89e32579811ac6
jeppeb91 <jeppeb91@hotmail.com>
2017-04-27 12:03:19 +0200

    Updated the comments

--- src/Driver.java
+++ src/Driver.java
@@ -10,14 +10,20 @@ public class Driver {
             //2. Create a statement
             Statement myStm = myConn.createStatement();
             //3. Execute sql query
-            //ResultSet myRs = myStm.executeQuery("SELECT * FROM confluence.ao_950dc3_tc_events;");
-            myStm.executeUpdate("INSERT INTO confluence.ao_950dc3_tc_events (ALL_DAY, CREATED, DESCRIPTION, END, ID, LAST_MODIFIED, LOCATION, ORGANISER, RECURRENCE_ID_TIMESTAMP, RECURRENCE_RULE, REMINDER_SETTING_ID, SEQUENCE, START, SUB_CALENDAR_ID, SUMMARY, URL, UTC_END, UTC_START, VEVENT_UID)\n" +
-                    "VALUES ('1', '1493235152154', '', '1493251200000', '5', '1493235152154', '', '4028b8815babae10015babb056780000', NULL, NULL, NULL, '0', '1493164800000', 'dfa1eb25-ef12-42c8-abcf-71dec96b58ac', 'Java-pushed event', NULL, '1493244000000', '1493157600000', '20170426T193232Z--2091550207@localhost')");
 
-            /*while (myRs.next()) {
+            //this section is to FETCH data
+            /*ResultSet myRs = myStm.executeQuery("SELECT * FROM confluence.ao_950dc3_tc_events;");
+            while (myRs.next()) {
                 System.out.println(myRs.getString("SUMMARY"));
                 //see sql queries for more info
             }*/
+
+            //This section is to PUSH Data
+            myStm.executeUpdate("INSERT INTO confluence.ao_950dc3_tc_events (ALL_DAY, CREATED, DESCRIPTION, END, ID, LAST_MODIFIED, LOCATION, ORGANISER, RECURRENCE_ID_TIMESTAMP, RECURRENCE_RULE, REMINDER_SETTING_ID, SEQUENCE, START, SUB_CALENDAR_ID, SUMMARY, URL, UTC_END, UTC_START, VEVENT_UID)\n" +
+                    "VALUES ('1', '1493235152154', '', '1493251200000', '5', '1493235152154', '', '4028b8815babae10015babb056780000', NULL, NULL, NULL, '0', '1493164800000', 'dfa1eb25-ef12-42c8-abcf-71dec96b58ac', 'Java-pushed event', NULL, '1493244000000', '1493157600000', '20170426T193232Z--2091550207@localhost')");
+
+
+
             myConn.close(); //closing connection
 
         } catch (Exception exc) {
